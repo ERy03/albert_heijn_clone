82d09ed9c961e6c91b6c66524cd1403857f3f9b3
Ryota <ryoenoki2013@gmail.com>
2023-08-25 23:43:24 +0200

    update: use material 3

--- lib/app.dart
+++ lib/app.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Albert Heijn Clone',
       theme: ThemeData(
+          useMaterial3: true,
           primarySwatch: Colors.blue,
           hoverColor: Colors.transparent,
           scaffoldBackgroundColor: Colors.white),
