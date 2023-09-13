008a37f3326d502d27e62a6fd982af1035a1c97e
Ryota <ryoenoki2013@gmail.com>
2023-09-13 23:27:38 +0200

    update: home this week section

--- lib/components/home_this_week.dart
+++ lib/components/home_this_week.dart
@@ -1,3 +1,6 @@
+import 'package:albert_heijn_clone/components/data/data.dart';
+import 'package:albert_heijn_clone/components/home_this_week_image_container.dart';
+import 'package:albert_heijn_clone/constant/colors.dart';
 import 'package:albert_heijn_clone/localization/hardcoded.dart';
 import 'package:flutter/material.dart';
 
@@ -6,69 +9,40 @@ class HomeThisWeekSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // TODO remove padding
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'This Week in Albert Heijn'.hardcoded,
-          style: const TextStyle(fontSize: 25),
-        ),
-        SizedBox(
-          height: 150,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [
-              Container(
-                width: 150,
-                color: Colors.red,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                width: 150,
-                color: Colors.green,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                width: 150,
-                color: Colors.green,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                width: 150,
-                color: Colors.green,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                width: 150,
-                color: Colors.green,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                width: 150,
-                color: Colors.green,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                width: 150,
-                color: Colors.green,
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16.0,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'This Week in Albert Heijn'.hardcoded,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: -0.5,
+              color: ahDarkBlue,
+            ),
           ),
-        )
-      ],
+          const SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+            height: 230,
+            child: ListView.builder(
+              itemCount: thisWeekData.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return HomeThisWeekImageContainer(
+                  image: thisWeekData[index]['image']!,
+                  title: thisWeekData[index]['title']!,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
