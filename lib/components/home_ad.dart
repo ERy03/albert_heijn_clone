165a6bd560172809ead905ecf963e425bf5148e3
Ryota <ryoenoki2013@gmail.com>
2023-09-17 21:28:51 +0200

    update: home ad component

--- lib/components/home_ad.dart
+++ lib/components/home_ad.dart
@@ -5,10 +5,13 @@ class HomeAd extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+    return GestureDetector(
+      onTap: () {
+        // TODO: Add navigation
+        print('tapped');
+      },
       child: Container(
-        width: 367,
+        margin: const EdgeInsets.symmetric(horizontal: 16.0),
         height: 138,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
@@ -20,32 +23,58 @@ class HomeAd extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Rich in proteins '),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Try Felix now',
-                        textAlign: TextAlign.left,
+              const SizedBox(
+                width: 170,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Rich in protein & full of flavor',
+                      maxLines: 2,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.30,
+                        color: Colors.black,
+                        overflow: TextOverflow.ellipsis,
+                        height: 1.6,
                       ),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 15,
-                      )
-                    ],
-                  )
-                ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Try Felix now',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            overflow: TextOverflow.ellipsis,
+                            height: 1.6,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 9,
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 11,
+                          weight: 10,
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.red,
+              Image.asset(
+                'assets/images/pet-food.png',
+                fit: BoxFit.cover,
+                alignment: Alignment.centerLeft,
+                height: 114,
+                width: 114,
               )
             ],
           ),
