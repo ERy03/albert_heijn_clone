753da30f8c49ffe204ae522c45acb89c6f940f8f
Ryota <ryoenoki2013@gmail.com>
2023-09-29 21:40:36 +0200

    add: bonus selection data

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,3 +1,5 @@
+import 'package:albert_heijn_clone/models/bonus_selection.dart';
+
 const thisWeekData = [
   {
     'title': 'Recipes for fresh tomato pasta',
@@ -11,4 +13,30 @@ const thisWeekData = [
     'title': 'New snacks that will sweeten your day',
     'image': 'assets/images/this_week_img3.jpg',
   },
+];
+
+List<BonusSelection> bonusSelectionData = [
+  BonusSelection(
+    title: 'All Ice cream',
+    description: "E.g. Magnum, Cornetto, Ben & Jerry's",
+    imagePath: 'assets/images/bonusSelection-1.png',
+    originalPrice: '5.98',
+    bonusPrice: '2.99',
+    bonusLabel: '1 + 1 FREE',
+  ),
+  BonusSelection(
+    title: 'All AH Yoghurt',
+    imagePath: 'assets/images/bonusSelection-2.png',
+    originalPrice: '1.99',
+    bonusPrice: '2.49',
+    bonusLabel: '25% OFF',
+  ),
+  BonusSelection(
+    title: 'All Cheese',
+    description: "Per Stock",
+    imagePath: 'assets/images/bonusSelection-3.png',
+    originalPrice: '3.09',
+    bonusPrice: '0.99',
+    bonusLabel: 'For 0.99',
+  ),
 ];
