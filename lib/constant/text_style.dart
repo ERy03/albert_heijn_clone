de149f375cb7c2ce1b55c5193e5cc4efbe7392df
Ryota <ryoenoki2013@gmail.com>
2023-10-02 21:20:09 +0200

    add: text style constant

---
+++ lib/constant/text_style.dart
@@ -0,0 +1,10 @@
+import 'package:albert_heijn_clone/constant/colors.dart';
+import 'package:flutter/material.dart';
+
+const TextStyle ahCarroisGothic = TextStyle(
+    fontFamily: 'CarroisGothic',
+    color: ahOrange,
+    fontSize: 16,
+    backgroundColor: Colors.red,
+    height: 0,
+    letterSpacing: -0.3);
