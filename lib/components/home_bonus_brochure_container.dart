e3111412ac29368225d7abd650ecf303c8e2a31c
Ryota <ryoenoki2013@gmail.com>
2023-10-02 22:31:17 +0200

    update: completed bonus brochure container

--- lib/components/home_bonus_brochure_container.dart
+++ lib/components/home_bonus_brochure_container.dart
@@ -12,6 +12,8 @@ class HomeBonusBrochureContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bonusPriceLeading = bonusSelection.bonusPrice.split('.')[0];
+    final bonusPriceTrailing = bonusSelection.bonusPrice.split('.')[1];
     return GestureDetector(
       onTap: () {
         // TODO
@@ -41,26 +43,103 @@ class HomeBonusBrochureContainer extends StatelessWidget {
                   children: [
                     // Image Part with bonus label and bonus price
                     Container(
-                      color: Colors.grey.shade300,
                       height: 140,
                       width: double.infinity,
-                      child: const Stack(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(bonusSelection.imagePath),
+                          fit: BoxFit.fitHeight,
+                        ),
+                      ),
+                      child: Stack(
                         alignment: Alignment.bottomLeft,
                         children: [
+                          // Orange Label
                           Positioned(
-                            child: Text('hello'),
+                            child: Container(
+                              height: 24,
+                              width: 104,
+                              decoration: BoxDecoration(
+                                color: ahOrange,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  bonusSelection.bonusLabel,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
+                          // Crossed out original price
+                          Positioned(
+                            bottom: 28,
+                            right: 0,
+                            child: Text(
+                              bonusSelection.originalPrice,
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 12,
+                                decoration: TextDecoration.lineThrough,
+                              ),
+                            ),
+                          ),
+                          // Bonus Price
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  bonusPriceLeading,
+                                  style: const TextStyle(
+                                    color: ahOrange,
+                                    height: 0.75,
+                                    fontSize: 36,
+                                    letterSpacing: -1,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 3.0),
+                                  child: Text(
+                                    bonusPriceTrailing,
+                                    style: const TextStyle(
+                                      color: ahOrange,
+                                      height: 0.75,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700,
+                                      letterSpacing: -0.5,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          // Dot to separate bonus price leading and trailing
+                          Positioned(
+                            bottom: 1,
+                            right: 18,
+                            child: Container(
+                              height: 6,
+                              width: 6,
+                              color: ahOrange,
+                            ),
+                          )
                         ],
                       ),
                     ),
                     // SizedBox
                     Container(
                       height: 9,
-                      color: Colors.red.shade100,
                     ),
                     // Text Part
-                    Container(
-                      color: Colors.grey.shade300,
+                    SizedBox(
                       height: 57,
                       width: double.infinity,
                       child: Column(
