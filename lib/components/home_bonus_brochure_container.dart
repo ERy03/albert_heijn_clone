10a2fe510859ce82c1f9305caff3467b6d3789f3
Ryota <ryoenoki2013@gmail.com>
2023-10-04 19:42:51 +0200

    update: use BorderContainerWithArrowIcon component

--- lib/components/home_bonus_brochure_container.dart
+++ lib/components/home_bonus_brochure_container.dart
@@ -1,4 +1,4 @@
-import 'package:albert_heijn_clone/components/rounded_arrow_icon.dart';
+import 'package:albert_heijn_clone/components/border_container_with_arrow_icon.dart';
 import 'package:albert_heijn_clone/constant/colors.dart';
 import 'package:albert_heijn_clone/constant/text_style.dart';
 import 'package:albert_heijn_clone/localization/hardcoded.dart';
@@ -14,179 +14,139 @@ class HomeBonusBrochureContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     final bonusPriceLeading = bonusSelection.bonusPrice.split('.')[0];
     final bonusPriceTrailing = bonusSelection.bonusPrice.split('.')[1];
-    return GestureDetector(
-      onTap: () {
-        // TODO
-        print('hello');
-      },
-      child: Container(
-        width: 228,
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.grey.shade300,
-            width: 1,
-          ),
-          borderRadius: BorderRadius.circular(4),
-        ),
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 12,
-                left: 15,
-                right: 15,
+    return BorderContainerWithArrowIcon(
+      child: Column(
+        children: [
+          // Image Part with bonus label and bonus price
+          Container(
+            height: 140,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(bonusSelection.imagePath),
+                fit: BoxFit.fitHeight,
               ),
-              child: SizedBox(
-                height: 206,
-                width: 197,
-                child: Column(
-                  children: [
-                    // Image Part with bonus label and bonus price
-                    Container(
-                      height: 140,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(bonusSelection.imagePath),
-                          fit: BoxFit.fitHeight,
+            ),
+            child: Stack(
+              alignment: Alignment.bottomLeft,
+              children: [
+                // Orange Label
+                Positioned(
+                  child: Container(
+                    height: 24,
+                    width: 104,
+                    decoration: BoxDecoration(
+                      color: ahOrange,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: Center(
+                      child: Text(
+                        bonusSelection.bonusLabel,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
                         ),
                       ),
-                      child: Stack(
-                        alignment: Alignment.bottomLeft,
-                        children: [
-                          // Orange Label
-                          Positioned(
-                            child: Container(
-                              height: 24,
-                              width: 104,
-                              decoration: BoxDecoration(
-                                color: ahOrange,
-                                borderRadius: BorderRadius.circular(4),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  bonusSelection.bonusLabel,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          // Crossed out original price
-                          Positioned(
-                            bottom: 28,
-                            right: 0,
-                            child: Text(
-                              bonusSelection.originalPrice,
-                              style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 12,
-                                decoration: TextDecoration.lineThrough,
-                              ),
-                            ),
-                          ),
-                          // Bonus Price
-                          Positioned(
-                            bottom: 0,
-                            right: 0,
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  bonusPriceLeading,
-                                  style: const TextStyle(
-                                    color: ahOrange,
-                                    height: 0.75,
-                                    fontSize: 36,
-                                    letterSpacing: -1,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 3.0),
-                                  child: Text(
-                                    bonusPriceTrailing,
-                                    style: const TextStyle(
-                                      color: ahOrange,
-                                      height: 0.75,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w700,
-                                      letterSpacing: -0.5,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          // Dot to separate bonus price leading and trailing
-                          Positioned(
-                            bottom: 1,
-                            right: 18,
-                            child: Container(
-                              height: 6,
-                              width: 6,
-                              color: ahOrange,
-                            ),
-                          )
-                        ],
-                      ),
                     ),
-                    // SizedBox
-                    Container(
-                      height: 9,
-                    ),
-                    // Text Part
-                    SizedBox(
-                      height: 57,
-                      width: double.infinity,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'today'.hardcoded,
-                            style: ahCarroisGothic,
-                          ),
-                          Text(
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            bonusSelection.title,
-                            style: ahCarroisGothic.copyWith(color: ahDarkBlue),
-                          ),
-                          if (bonusSelection.description != null &&
-                              bonusSelection.description != '') ...[
-                            Text(
-                              bonusSelection.description ?? '',
-                              style: ahCarroisGothic.copyWith(
-                                color: const Color(0xFF647887),
-                                fontSize: 12,
-                                letterSpacing: 0.1,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ],
-                        ],
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                // Crossed out original price
+                Positioned(
+                  bottom: 28,
+                  right: 0,
+                  child: Text(
+                    bonusSelection.originalPrice,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 12,
+                      decoration: TextDecoration.lineThrough,
+                    ),
+                  ),
+                ),
+                // Bonus Price
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        bonusPriceLeading,
+                        style: const TextStyle(
+                          color: ahOrange,
+                          height: 0.75,
+                          fontSize: 36,
+                          letterSpacing: -1,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 3.0),
+                        child: Text(
+                          bonusPriceTrailing,
+                          style: const TextStyle(
+                            color: ahOrange,
+                            height: 0.75,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                            letterSpacing: -0.5,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // Dot to separate bonus price leading and trailing
+                Positioned(
+                  bottom: 1,
+                  right: 18,
+                  child: Container(
+                    height: 6,
+                    width: 6,
+                    color: ahOrange,
+                  ),
+                )
+              ],
             ),
-            // Orange Rounded Arrow Button
-            const Positioned(
-              bottom: 12,
-              right: 12,
-              child: RoundedArrowIcon(
-                border: true,
-                color: ahOrange,
-                height: 25,
-                width: 25,
-              ),
+          ),
+          // SizedBox
+          Container(
+            height: 9,
+          ),
+          // Text Part
+          SizedBox(
+            height: 57,
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'today'.hardcoded,
+                  style: ahCarroisGothic,
+                ),
+                Text(
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  bonusSelection.title,
+                  style: ahCarroisGothic.copyWith(color: ahDarkBlue),
+                ),
+                if (bonusSelection.description != null &&
+                    bonusSelection.description != '') ...[
+                  Text(
+                    bonusSelection.description ?? '',
+                    style: ahCarroisGothic.copyWith(
+                      color: const Color(0xFF647887),
+                      fontSize: 12,
+                      letterSpacing: 0.1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
