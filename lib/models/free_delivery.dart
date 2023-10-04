10a8dcf70fae2fedf152dbcdec3d9b223d1aaec9
Ryota <ryoenoki2013@gmail.com>
2023-10-04 19:40:13 +0200

    add: FreeDelivery model

---
+++ lib/models/free_delivery.dart
@@ -0,0 +1,11 @@
+class FreeDelivery {
+  final String title;
+  final String imagePath;
+  final String bonusLabel;
+
+  FreeDelivery({
+    required this.title,
+    required this.imagePath,
+    required this.bonusLabel,
+  });
+}
