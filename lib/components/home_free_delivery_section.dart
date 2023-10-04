f06ba6b990a1f3194e6075e80bcf75e1bb12f120
Ryota <ryoenoki2013@gmail.com>
2023-10-04 19:41:50 +0200

    update: replace with freeDeliveryData

--- lib/components/home_free_delivery_section.dart
+++ lib/components/home_free_delivery_section.dart
@@ -1,4 +1,6 @@
+import 'package:albert_heijn_clone/components/home_free_delivery_container.dart';
 import 'package:albert_heijn_clone/constant/colors.dart';
+import 'package:albert_heijn_clone/data/data.dart';
 import 'package:albert_heijn_clone/localization/hardcoded.dart';
 import 'package:flutter/material.dart';
 
@@ -26,23 +28,23 @@ class HomeFreeDeliverySection extends StatelessWidget {
           const SizedBox(
             height: 25,
           ),
-          // SizedBox(
-          //   height: 258,
-          //   child: ListView.separated(
-          //     padding: const EdgeInsets.only(
-          //       right: 16,
-          //     ),
-          //     scrollDirection: Axis.horizontal,
-          //     itemBuilder: (context, index) {
-          //       return HomeBonusBrochureContainer(
-          //           bonusSelection: bonusSelectionData[index]);
-          //     },
-          //     separatorBuilder: (context, index) => const SizedBox(
-          //       width: 10,
-          //     ),
-          //     itemCount: bonusSelectionData.length,
-          //   ),
-          // )
+          SizedBox(
+            height: 258,
+            child: ListView.separated(
+              padding: const EdgeInsets.only(
+                right: 16,
+              ),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return HomeFreeDeliveryContainer(
+                    freeDelivery: freeDeliveryData[index]);
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 10,
+              ),
+              itemCount: freeDeliveryData.length,
+            ),
+          )
         ],
       ),
     );
