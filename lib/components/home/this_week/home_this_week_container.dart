460fc16bc7329c8c70da6de372428e8f28a60aa2
Ryota <ryoenoki2013@gmail.com>
2023-10-06 17:49:00 +0200

    update: home this week container

--- lib/components/home/this_week/home_this_week_container.dart
+++ lib/components/home/this_week/home_this_week_container.dart
@@ -1,15 +1,14 @@
 import 'package:albert_heijn_clone/components/common/rounded_arrow_icon.dart';
+import 'package:albert_heijn_clone/models/this_week_model.dart';
 import 'package:flutter/material.dart';
 
 class HomeThisWeekContainer extends StatelessWidget {
   const HomeThisWeekContainer({
     Key? key,
-    required this.image,
-    required this.title,
+    required this.thisWeek,
   }) : super(key: key);
 
-  final String image;
-  final String title;
+  final ThisWeekModel thisWeek;
 
   @override
   Widget build(BuildContext context) {
@@ -23,36 +22,46 @@ class HomeThisWeekContainer extends StatelessWidget {
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
           image: DecorationImage(
-            image: AssetImage(image),
+            image: AssetImage(thisWeek.backgroundImage),
             fit: BoxFit.cover,
           ),
         ),
         child: Stack(
           children: [
-            Positioned(
-              top: 10,
-              left: 15,
-              child: SizedBox(
-                width: 290,
-                child: Text(
-                  title,
-                  maxLines: 2,
-                  style: const TextStyle(
-                    shadows: [
-                      Shadow(
-                        color: Colors.black,
-                        offset: Offset(1, 1),
-                        blurRadius: 1,
-                      ),
-                    ],
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: -0.4,
-                    color: Colors.white,
-                    overflow: TextOverflow.ellipsis,
-                    height: 1.6,
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 13,
+                left: 16,
+                right: 16,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    thisWeek.title,
+                    maxLines: 2,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: -0.4,
+                      color: Colors.white,
+                      overflow: TextOverflow.ellipsis,
+                      height: 1.6,
+                    ),
                   ),
-                ),
+                  Container(
+                    padding: const EdgeInsets.only(
+                      bottom: 10,
+                    ),
+                    width: 290,
+                    height: 153,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(thisWeek.image),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             const Positioned(
