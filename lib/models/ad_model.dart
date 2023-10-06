2cfa27f040aa2e1719d091e9febabf9a9332936f
Ryota <ryoenoki2013@gmail.com>
2023-10-06 17:41:20 +0200

    add: ad model

---
+++ lib/models/ad_model.dart
@@ -0,0 +1,11 @@
+class AdModel {
+  final String title;
+  final String imagePath;
+  final String actionText;
+
+  AdModel({
+    required this.title,
+    required this.imagePath,
+    required this.actionText,
+  });
+}
