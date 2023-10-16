99fd5e4c0426f7c49d881eb14f303d9dfc4f598f
Ryota <ryoenoki2013@gmail.com>
2023-10-16 19:37:42 +0200

    wip: add products type section

---
+++ lib/components/products/products_types/products_types_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductsTypesSection extends StatelessWidget {
+  const ProductsTypesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('hello');
+  }
+}
