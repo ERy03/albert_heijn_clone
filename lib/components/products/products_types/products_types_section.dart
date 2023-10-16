269afc7b87bc8251c885e8fc1ad3c499f096e650
Ryota <ryoenoki2013@gmail.com>
2023-10-16 22:42:26 +0200

    wip: products type sections

--- lib/components/products/products_types/products_types_section.dart
+++ lib/components/products/products_types/products_types_section.dart
@@ -1,3 +1,4 @@
+import 'package:albert_heijn_clone/data/data.dart';
 import 'package:flutter/material.dart';
 
 class ProductsTypesSection extends StatelessWidget {
@@ -5,6 +6,25 @@ class ProductsTypesSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('hello');
+    return SizedBox(
+      height: 150,
+      child: ListView.separated(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+        ),
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Container(
+            height: double.infinity,
+            width: 200,
+            color: Colors.red,
+          );
+        },
+        separatorBuilder: (context, index) => const SizedBox(
+          width: 8,
+        ),
+        itemCount: productsTypesData.length,
+      ),
+    );
   }
 }
