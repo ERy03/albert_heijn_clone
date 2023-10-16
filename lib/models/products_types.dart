d4cd67180167feb001376a22b046bf7e0f39bcda
Ryota <ryoenoki2013@gmail.com>
2023-10-16 22:41:54 +0200

    add: products types model

---
+++ lib/models/products_types.dart
@@ -0,0 +1,11 @@
+class ProductsTypesModel {
+  final String type;
+  final String description;
+  final String image;
+
+  ProductsTypesModel({
+    required this.type,
+    required this.description,
+    required this.image,
+  });
+}
