107d26f39adf8395661e7143134ba809ff573964
Ryota <ryoenoki2013@gmail.com>
2023-10-18 23:05:24 +0200

    wip: use custom scroll view with sliver

---
+++ lib/components/products/categories/categories_section.dart
@@ -0,0 +1,49 @@
+import 'package:albert_heijn_clone/constant/colors.dart';
+import 'package:albert_heijn_clone/localization/hardcoded.dart';
+import 'package:flutter/material.dart';
+
+class CategoriesSection extends StatelessWidget {
+  const CategoriesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverGrid.count(
+        crossAxisCount: 2,
+        children: List.generate(10, (index) {
+          return Container(
+            height: 20,
+            color: Colors.blue,
+            child: Center(
+              child: Text(
+                'Item $index',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          );
+        }),
+        // SizedBox(
+        //   height: 258,
+        //   child: ListView.separated(
+        //     padding: const EdgeInsets.symmetric(
+        //       horizontal: 16,
+        //     ),
+        //     scrollDirection: Axis.horizontal,
+        //     itemBuilder: (context, index) {
+        //       return HomeFreeDeliveryContainer(
+        //           freeDelivery: freeDeliveryData[index]);
+        //     },
+        //     separatorBuilder: (context, index) => const SizedBox(
+        //       width: 10,
+        //     ),
+        //     itemCount: freeDeliveryData.length,
+        //   ),
+        // )
+      ),
+    );
+  }
+}
--- lib/screens/products.dart
+++ lib/screens/products.dart
@@ -1,6 +1,9 @@
 import 'package:albert_heijn_clone/components/common/appbar.dart';
 import 'package:albert_heijn_clone/components/common/search_bar_button.dart';
+import 'package:albert_heijn_clone/components/products/categories/categories_section.dart';
 import 'package:albert_heijn_clone/components/products/products_types/products_types_section.dart';
+import 'package:albert_heijn_clone/constant/colors.dart';
+import 'package:albert_heijn_clone/localization/hardcoded.dart';
 import 'package:flutter/material.dart';
 
 class ProductsScreen extends StatelessWidget {
@@ -10,26 +13,95 @@ class ProductsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const HomeAppBar(),
-      body: SingleChildScrollView(
-          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              HomeSearchBarButton(
-                onPressed: () {},
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              // Type of recipes section
-              const ProductsTypesSection(),
-              const SizedBox(
-                height: 28,
-              ),
+      body: CustomScrollView(
+        slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+            sliver: SliverList(
+              delegate: SliverChildListDelegate([
+                HomeSearchBarButton(
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                //  Type of recipes section
+                const ProductsTypesSection(),
+                const SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0),
+                  child: Text(
+                    'Categories'.hardcoded,
+                    style: const TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: -0.5,
+                      color: ahDarkBlue,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+              ]),
+            ),
+          ),
+          const CategoriesSection(),
 
-              // Categories section
-            ],
-          )),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 50.0,
+              width: double.infinity,
+              color: Colors.yellow,
+            ),
+          ),
+          // SliverToBoxAdapter(
+          //   child: Padding(
+          //       padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+          //       child: Column(
+          //         crossAxisAlignment: CrossAxisAlignment.start,
+          //         children: [
+          //           HomeSearchBarButton(
+          //             onPressed: () {},
+          //           ),
+          //           const SizedBox(
+          //             height: 30,
+          //           ),
+          //           // Type of recipes section
+          //           const ProductsTypesSection(),
+          //           const SizedBox(
+          //             height: 28,
+          //           ),
+          //           // Categories section
+          //           const CategoriesSection(),
+          //         ],
+          //       )),
+          // ),
+
+          // Padding(
+          //     padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+          //     child: Column(
+          //       crossAxisAlignment: CrossAxisAlignment.start,
+          //       children: [
+          //         HomeSearchBarButton(
+          //           onPressed: () {},
+          //         ),
+          //         const SizedBox(
+          //           height: 30,
+          //         ),
+          //         // Type of recipes section
+          //         const ProductsTypesSection(),
+          //         const SizedBox(
+          //           height: 28,
+          //         ),
+          //         // Categories section
+          //         const CategoriesSection(),
+          //       ],
+          //     )),
+        ],
+      ),
     );
   }
 }
