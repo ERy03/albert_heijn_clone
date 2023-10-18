e6b4764b7db8f7801274b219f1d44ff05154610b
Ryota <ryoenoki2013@gmail.com>
2023-10-18 17:02:27 +0200

    update: products types model

--- lib/models/products_types.dart
+++ lib/models/products_types.dart
@@ -1,11 +1,15 @@
 class ProductsTypesModel {
   final String type;
   final String description;
+  final String backgroundImage;
+  final String? icon;
   final String image;
 
   ProductsTypesModel({
     required this.type,
     required this.description,
+    required this.backgroundImage,
     required this.image,
+    this.icon,
   });
 }
