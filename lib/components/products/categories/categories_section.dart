716077202e7c0753c81f8d0f79f104aee8759069
Ryota <ryoenoki2013@gmail.com>
2023-10-25 21:31:59 +0200

    update: categories section

--- lib/components/products/categories/categories_section.dart
+++ lib/components/products/categories/categories_section.dart
@@ -1,4 +1,6 @@
 import 'package:albert_heijn_clone/components/products/categories/categories_card.dart';
+import 'package:albert_heijn_clone/data/data.dart';
+import 'package:albert_heijn_clone/models/categories_model.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesSection extends StatelessWidget {
@@ -8,29 +10,36 @@ class CategoriesSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverPadding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      sliver: SliverGrid.count(
-        crossAxisCount: 2,
-        children: List.generate(10, (index) {
-          return const CategoriesCard();
-        }),
-        // SizedBox(
-        //   height: 258,
-        //   child: ListView.separated(
-        //     padding: const EdgeInsets.symmetric(
-        //       horizontal: 16,
-        //     ),
-        //     scrollDirection: Axis.horizontal,
-        //     itemBuilder: (context, index) {
-        //       return HomeFreeDeliveryContainer(
-        //           freeDelivery: freeDeliveryData[index]);
-        //     },
-        //     separatorBuilder: (context, index) => const SizedBox(
-        //       width: 10,
-        //     ),
-        //     itemCount: freeDeliveryData.length,
-        //   ),
-        // )
-      ),
+      sliver: SliverGrid.count(crossAxisCount: 2, children: [
+        CategoriesCard(
+          categories: CategoriesModel(
+            imagePath: 'assets/images/order-history.png',
+            category: Categories.previous,
+          ),
+          height: 45,
+        ),
+        for (int i = 0; i < categoriesData.length; i++)
+          CategoriesCard(
+            categories: categoriesData[i],
+          ),
+      ]),
+      // SizedBox(
+      //   height: 258,
+      //   child: ListView.separated(
+      //     padding: const EdgeInsets.symmetric(
+      //       horizontal: 16,
+      //     ),
+      //     scrollDirection: Axis.horizontal,
+      //     itemBuilder: (context, index) {
+      //       return HomeFreeDeliveryContainer(
+      //           freeDelivery: freeDeliveryData[index]);
+      //     },
+      //     separatorBuilder: (context, index) => const SizedBox(
+      //       width: 10,
+      //     ),
+      //     itemCount: freeDeliveryData.length,
+      //   ),
+      // )
     );
   }
 }
